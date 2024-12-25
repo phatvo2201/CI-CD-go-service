11f8ad1956f2760931ec05efaecdeb8f3400217b
qlack <qlack@192.168.x.x.non-exists.ptr.local>
2024-12-25 22:39:01 +0700

    source to build image can comunicate with db

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 set -e
+echo "start source env file copy from secret aws"
+source /app/app.env
+
 echo "start run db migration in docker"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
